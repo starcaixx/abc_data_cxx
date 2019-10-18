145980b6bb259603e1041cad44fc7c7f50b27334
caixingxing <xingxingcai@huangbaoche.com>
2019-10-18 14:59:39 +0800

    avro2hive_test

--- abc_data_cxx/avro2hive_mr/src/main/java/com/cxlx/avro2hive/Avro2Hive.java
+++ abc_data_cxx/avro2hive_mr/src/main/java/com/cxlx/avro2hive/Avro2Hive.java
@@ -1,5 +1,6 @@
 package com.cxlx.avro2hive;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.aliyun.oss.OSSClient;
 import org.apache.avro.generic.GenericRecord;
@@ -7,6 +8,7 @@ import org.apache.avro.mapred.AvroKey;
 import org.apache.avro.mapreduce.AvroKeyInputFormat;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
@@ -30,7 +32,7 @@ import java.util.*;
  * @author star
  * @create 2019-09-09 15:21
  */
-public class Avro2Hive extends Configured implements Tool  {
+public class Avro2Hive extends Configured implements Tool {
     private Configuration conf = null;
     private static final Logger logger = LoggerFactory.getLogger(Avro2Hive.class);
 
@@ -53,7 +55,7 @@ public class Avro2Hive extends Configured implements Tool  {
         conf.set("inpath", args[0]);
         conf.set("outpath", args[1]);
         conf.set("logdate", args[2]);
-        conf.set("basiceventinfooutpath", args[1]+"basiceventinfo/dt="+args[2]+"/");
+        conf.set("basiceventinfooutpath", args[1] + "basiceventinfo/dt=" + args[2] + "/");
 
         Job job = Job.getInstance(conf, this.getClass().getSimpleName());
 
@@ -71,10 +73,9 @@ public class Avro2Hive extends Configured implements Tool  {
         job.setInputFormatClass(AvroKeyInputFormat.class);
         AvroKeyInputFormat.setMaxInputSplitSize(job, 1024 * 1024 * 128);
 
-//        AvroKeyInputFormat.setInputPaths(job,new Path(args[0]));
+//        AvroKeyInputFormat.setInputPaths(job, new Path(args[0]));
         initInpath(job);
         initOutpath(job);
-
 
 
         //通过此配置可以不再产生默认的空文件
@@ -111,14 +112,13 @@ public class Avro2Hive extends Configured implements Tool  {
             //传入的是文件
             if (split[split.length - 1].contains(".")) {
                 AvroKeyInputFormat.addInputPath(job, new Path(inpath));
-            //传入的是目录
-            }else {
-                if (ossClient.doesObjectExist(split[2], split[3] + "/" + split[4] + "/" + split[5] + "/")) {
-                    StringBuilder inPath = new StringBuilder();
+                //传入的是目录
+            } else {
+                if (ossClient.doesObjectExist(split[2], split[3] + "/" + split[4] + "/" + split[5] + "/", false)) {
                     List<String> lists = EMapReduceOSSUtil.listCompleteUri(split[2], split[3] + "/" + split[4] + "/" + split[5] + "/", conf, false);
                     for (String list : lists) {
-                        AvroKeyInputFormat.addInputPath(job, new Path(inpath+list.substring(list.lastIndexOf("/")+1)));
-                        logger.info("uri:" + inpath+list.substring(list.lastIndexOf("/")+1));
+                        AvroKeyInputFormat.addInputPath(job, new Path(inpath + list.substring(list.lastIndexOf("/") + 1)));
+                        logger.info("uri:" + inpath + list.substring(list.lastIndexOf("/") + 1));
                     }
                 } else {
                     throw new RuntimeException(inpath + "not exists!");
@@ -143,7 +143,9 @@ public class Avro2Hive extends Configured implements Tool  {
 
     public static class AvroLogMapper extends Mapper<AvroKey<GenericRecord>, NullWritable, Text, Text> {
         MultipleOutputs<Text, Text> multipleOutputs = null;
-        private static volatile Map<String, ArrayList<String>> structTableMap = HiveJdbcUtil.getTableInfo();
+        FileSystem fs = null;
+        private static volatile Map<String, List<String>> structTableMap = null;
+
         Text eventType = new Text();
         Text json = new Text();
         StringBuilder record = new StringBuilder();
@@ -151,9 +153,28 @@ public class Avro2Hive extends Configured implements Tool  {
         @Override
         protected void setup(Context context) throws IOException, InterruptedException {
             logger.info("enter map setup=============================");
-
+            Configuration conf = context.getConfiguration();
             multipleOutputs = new MultipleOutputs<Text, Text>(context);
 
+            if (structTableMap == null) {
+                synchronized ("hello") {
+//                    BufferedReader br = new BufferedReader(new FileReader())
+                    fs = FileSystem.get(conf);
+                    FSDataInputStream fis = fs.open(new Path("/tmp/cacheschema.txt"));
+
+                    byte[] bys = new byte[1024 * 8];
+
+                    StringBuilder sb = new StringBuilder();
+                    int length = 0;
+                    while ((length = fis.read(bys)) != -1) {
+                        String s = new String(bys, 0, length);
+                        sb.append(s);
+                    }
+
+                    structTableMap = JSON.parseObject(sb.toString(), Map.class);
+                }
+                System.out.println(structTableMap.keySet().toString());
+            }
         }
 
         @Override
@@ -161,39 +182,36 @@ public class Avro2Hive extends Configured implements Tool  {
             record.delete(0, record.length());
             GenericRecord datum = key.datum();
 
+//            System.out.println(structTableMap.keySet().toString());
+//            logger.error("==========================="+key.datum()+"");
             JSONObject jsonGene = JSONObject.parseObject(datum.toString());
+            eventType.set(datum.get("eventType") + "");
 
-            Object eventType = jsonGene.get("eventType");
-            if (!structTableMap.containsKey(eventType)) {
-                logger.error("datum:"+datum+":eventType:"+eventType);
+            if (!structTableMap.containsKey(eventType + "")) {
+                logger.error("eventType:" + eventType + ",datum:" + structTableMap.get(eventType + ""));
                 return;
             }
-            ArrayList<String> cols = structTableMap.get(eventType+"");
+
+            List<String> cols = structTableMap.get(eventType + "");
             Iterator<String> it = structTableMap.keySet().iterator();
-//            logger.info(jsonGene.toString());
             for (String col : cols) {
                 JSONObject jsonObject = null;
 
                 JSONObject object = jsonGene;
                 while (col.contains(".")) {
                     int i = col.indexOf(".");
-                    object = JSONObject.parseObject(object.get(col.substring(0,i))+"");
+                    object = JSONObject.parseObject(object.get(col.substring(0, i)) + "");
                     if (object == null) {
                         break;
                     }
-                    col = col.substring(i+1);
+                    col = col.substring(i + 1);
                 }
                 Object colSimp = "\\N";
                 if (object != null) {
                     colSimp = object.get(col);
                 }
-//                logger.info(col+"::"+colSimp+"::"+object);
-                record.append(col+":"+colSimp).append("\001");
+                record.append(col + ":" + colSimp).append("\001");
             }
-//            cols
-//            this.eventType.set(eventType + "");
-
-
 
             json.set(record + "");
             context.write(this.eventType, json);
@@ -202,6 +220,9 @@ public class Avro2Hive extends Configured implements Tool  {
         @Override
         protected void cleanup(Context context) throws IOException, InterruptedException {
             super.cleanup(context);
+            if (fs != null) {
+                fs.close();
+            }
         }
     }
 
